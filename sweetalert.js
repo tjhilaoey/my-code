100c13f85c848a0863eddaa5e4d51c2835157baf
tjhilaoey <125296205+tjhilaoey@users.noreply.github.com>
2023-02-14 00:29:19 +0700

    Create sweetalert.js

---
+++ sweetalert.js
@@ -0,0 +1,8 @@
+function popupku() {
+  var body = document.querySelector("body");      
+  var pWm = document.createElement("a");
+  pWm.setAttribute("href", "Tjhila Oey");
+  pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
+  pWm.innerHTML = "Tjhila Oey";
+  body.appendChild(pWm);
+}
